5f7607f393546a33381c2a950d08928000f1975c
John <mr.ma454xy@gmail.com>
2026-08-11 14:06:59 +0330

    Add files via upload

---
+++ search_audio.js
@@ -0,0 +1,49 @@
+let rec;
+let stream;
+export async function getAmedia(){
+    try{let stream = await navigator.mediaDevices.getUserMedia({ audio: true });}
+catch(error){
+    console.log("the user don't give acsses to microfon:  " + error)
+}}
+
+export async function startOpusRecording() {
+    try{var rec = new Recorder(stream, {
+            encoderPath: 'encoderWorker.min.js', 
+            numberOfChannels: 1 
+        });
+
+        rec.ondataavailable = function(typedArray) {
+        const blob = new Blob([typedArray], { type: 'audio/ogg' });
+        const audioUrl = URL.createObjectURL(blob);
+        const audio = document.getElementById('audioPlayer');
+        audio.src = audioUrl;
+        console.log("Audio recorded successfully!");
+
+        const formData = new FormData();
+        formData.append('audioFile', blob, `recorded_voice_${filename}.ogg`);
+        fetch('https://your-website.com/upload.php', { 
+                method: 'POST',
+                body: formData
+            })
+        }
+        rec.start()
+        console.log("در حال ضبط...");
+
+        setTimeout(()=>{
+            startOpusRecording()
+            },10000)}
+        catch(error){
+            console.log("error in recording:  " + error)}
+    
+}
+
+export async function stopOpusRecording() {
+    if(rec){
+        rec.stop()
+        console.log("recording is stop")
+    }
+
+
+
+
+}
